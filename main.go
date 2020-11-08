d4618e4455da05850015b684283d65ff1a0c8c15
Andrew Arrow <oneone@gmail.com>
2020-11-08 12:37:11 -0800

    ecs

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ecs")
+	fmt.Println("")
+	fmt.Println("  ecs ls            # list students at ecs")
+	fmt.Println("  ecs add           # add a new student")
+	fmt.Println("  ecs note          # add a note to a specific student")
+	fmt.Println("")
+}
